ef6b03c356306a6aa79d5a64c9f7e32601b87b6a
Sarita Kumari <sarita_kumari@persistent.com>
2026-05-20 16:08:47 +0530

    sign/signup page Login and Header page

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NETFLIX_LOGO } from '../constants/ImageAddress';
+
+const Header = () => {
+  return (
+    <div className="absolute z-30 bg-gradient-to-b from-black">
+        <img className="w-48" src={NETFLIX_LOGO} 
+        alt='Netflix'
+        />
+    </div>
+  )
+}
+
+export default Header;
